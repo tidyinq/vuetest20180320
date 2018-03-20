894ed155d7b23b09ee5db4ea948cb3e0a4f799a9
tidyinq <tidyinq@gmail.com>
2018-03-20 19:31:00 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <p v-if="seen">现在你看到我了{{ seen }}</p>
+    <img width="25%" src="./assets/logo.png">    
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  data: function() {
+    return { seen: 1 };
+  },
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
